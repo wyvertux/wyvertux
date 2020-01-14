710d46dca6fa2301e307489efb3ef482e6065f1a
Muhammad Herdiansyah <herdiansyah@netc.eu>
2020-01-14 00:43:23 +0000

    kiss: consistent repo PATH

--- core/kiss/files/kiss_path.sh
+++ core/kiss/files/kiss_path.sh
@@ -1,1 +1,1 @@
-export KISS_PATH=/var/db/kiss/wyvertux/core:/var/db/kiss/repo/extra-toolchain:/var/db/kiss/repo/extra-boot:/var/db/kiss/wyvertux/extra:/var/db/kiss/repo/gnu
+export KISS_PATH=/var/db/kiss/wyvertux/core:/var/db/kiss/wyvertux/extra-toolchain:/var/db/kiss/wyvertux/extra-boot:/var/db/kiss/wyvertux/extra:/var/db/kiss/wyvertux/gnu
