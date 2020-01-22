84624d55c6cb0ebb849eaea81e81600e100adc4c
Muhammad Herdiansyah <herdiansyah@netc.eu>
2020-01-22 01:02:01 +0000

    kiss: change PATH

--- core/kiss/files/kiss_path.sh
+++ core/kiss/files/kiss_path.sh
@@ -1,1 +1,1 @@
-export KISS_PATH=/var/db/kiss/wyvertux/core:/var/db/kiss/wyvertux/extra-toolchain:/var/db/kiss/wyvertux/extra-boot:/var/db/kiss/wyvertux/extra:/var/db/kiss/wyvertux/gnu
+export KISS_PATH=/var/db/kiss/wyvertux/core:/var/db/kiss/wyvertux/extra:/var/db/kiss/wyvertux/gnu
